27cc47d6c9ea1aef67ebb5183484a375f9675287
Joey Cho <jaeyoung.cho@tum.de>
2024-10-17 17:41:49 +0200

    add ISO IR 149

--- pkg/charset/charset.go
+++ pkg/charset/charset.go
@@ -64,6 +64,7 @@ var htmlEncodingNames = map[string]string{
 	"ISO_IR 148":      "iso-ir-148",
 	"ISO 2022 IR 148": "iso-ir-148",
 	"ISO_IR 149":      "euc-kr",
+	"ISO IR 149":      "euc-kr",
 	"ISO 2022 IR 149": "euc-kr",
 	"ISO 2022 IR 159": "iso-2022-jp",
 	"ISO_IR 166":      "iso-ir-166",
